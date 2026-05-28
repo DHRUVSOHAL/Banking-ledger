5949dc8fabc21c3b3259c26a433daefe32bc2ba3
Dhruv Sohal <sohaldhruv00@gmail.com>
2026-05-28 21:51:15 +0530

    initial commit

---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose')
+
+
+
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_DB_URL)
+        console.log('Connected to database')
+    }
+    catch(error){
+        console.log(error)
+    }
+
+}
+
+module.exports=connectDB
