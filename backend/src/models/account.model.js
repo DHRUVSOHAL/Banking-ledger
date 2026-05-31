0890bc67bf802c5be517bcec14876d205a4c58a8
Dhruv Sohal <sohaldhruv00@gmail.com>
2026-05-31 15:39:16 +0530

    create account2

--- backend/src/models/account.model.js
+++ backend/src/models/account.model.js
@@ -13,9 +13,10 @@ const accountSchema = new mongoose.Schema({
         type: String,
         enum: {
             values: ["ACTIVE", "FROZEN", "CLOSED"],
-            message: "Status can be either ACTIVE, FROZEN or CLOSED",
+            message: "Status can be either ACTIVE, FROZEN or CLOSED"
+        },
             default: "ACTIVE"
-        }
+        
     },
     currency: {
         type: String,
@@ -24,7 +25,7 @@ const accountSchema = new mongoose.Schema({
     },
 
 }, {
-    timestampes: true
+    timestamps: true
 })
 
 accountSchema.index({ user: 1, status: 1 })//compound index for faster retrieval of accounts by user and status
